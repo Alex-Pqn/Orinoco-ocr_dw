c97f7cea1f91d6dfa3e2bb9285f1f220476bbe56
Alexandre P <@paquienalexandre0@gmail.com>
2020-07-02 01:40:32 +0200

    Creation dynamic page product

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,58 @@
 
-let xhr = new XMLHttpRequest(); //Creation XML HTTP REQUEST
+// declaration variables
+let prdctImageUrl = document.querySelector("article.dyn-products__img");
+let prdctTitle = document.getElementById("dyn-products__name");
+let prdctDescription = document.getElementById("dyn-products__desc");
+let prdctPrice = document.getElementById("dyn-products__price");
+let prdctdLenses = document.getElementById("dyn-products__lenses");
 
-xhr.onreadystatechange = function() {
-    console.log(this);
-    if (this.readyState == 4 && this.status == 200) {
-        console.log(this.response) //return response API
+// CALL, REQUESTS API - GET
+// => http://localhost:3000/api/cameras/
 
-    } else if (this.status != 200 && this.status != 0) {
-        alert("Malheureuseument, l'API Camera n'a pas pu être récupérée...");
-        alert("Statut HTTP : " + this.status);
-    }
-};
-
-xhr.open("GET", "http://127.0.0.1:5500/models/Camera.js", true); //preparation request
-xhr.responseType = "object"; //modify type request in text
-xhr.send(); //send request
-
+const apiProducts = async function () {
+    let xhr = new XMLHttpRequest(); // XHR creation
+    xhr.open("GET", "http://localhost:3000/api/cameras/", true); // request preparation
+    xhr.responseType = "json"; // request type modify
+    xhr.send(); // send request
+    console.log(this); //return requests
+    xhr.onreadystatechange = function() { // requests checker
+        console.log(this);
+        if (this.readyState == 4 && this.status == 200 && xhr.DONE) {
+            let data = this.response;
+            console.log(data)
+            for (i = 0; i < data.length; i++) {
+                function displayContent () { // function containing my products (for display)
+                    const product = function (description, imageUrl, lenses, title, price, id) {
+                        this.id = id;
+                        this.imageUrl = imageUrl;
+                        this.title = title;
+                        this.description = description;
+                        this.price = price;
+                        this.lenses = lenses;
+                    }
+                    const cameras = new product (
+                        data[i]._id,
+                        data[i]._imageUrl,
+                        data[i]._title,
+                        data[i]._description,
+                        data[i]._price,
+                        data[i]._lenses,
+                    )
+                    // imageUrl.setAttribute("src", "./public/img/vcam_1.jpg")
+                    prdctImageUrl.setAttribute("src", product.imageUrl)
+                    prdctTitle.textContent = (product.title);
+                    prdctDescription.textContent = (product.description);
+                    prdctPrice.textContent = (product.price);
+                    prdctdLenses.textContent = (product.lenses);
+                }
+                displayContent();
+            }
+    
+        } else if (this.status != 200 && this.status != 0) {
+            alert("l'API Camera n'a malheureusement pas pu être récupérée...");
+            alert("Statut HTTP : " + this.status + ", état readyState : " + this.readyState);
+            alert("Veuillez réessayer ultérieurement.")
+        }
+    };
+}
+apiProducts();
